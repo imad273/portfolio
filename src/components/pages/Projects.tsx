fd6e6801661fa18f1078baa2f4dcd8dc4f928f66
imad273 <abbadimad0123@gmail.com>
2024-04-17 19:58:59 +0100

    feat: projects page

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import FyloPic from '@/assets/fylo.jpeg'
+import IpAddressTracking from '@/assets/ip-address-tracking.jpeg'
+import Ditch from '@/assets/Ditch.jpeg'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FaExternalLinkAlt } from "react-icons/fa";
@@ -13,16 +15,16 @@ const Projects = () => {
       link: "https://imad273.github.io/Fylo-Landing-Page/public/"
     },
     {
-      picture: FyloPic,
-      name: "Fylo",
-      description: "Fylo is a landing page",
-      link: "https://imad273.github.io/Fylo-Landing-Page/public/"
+      picture: IpAddressTracking,
+      name: "IP Address Tracker",
+      description: "Web application for tracking IP addresses and know where exactly they are, through an API called ipgoelocation.",
+      link: "https://imad273.github.io/ip-address-tracker/"
     },
     {
-      picture: FyloPic,
-      name: "Fylo",
-      description: "Fylo is a landing page",
-      link: "https://imad273.github.io/Fylo-Landing-Page/public/"
+      picture: Ditch,
+      name: "Ditch Portfolio",
+      description: "Portfolio for UI/UX designer using TailwindCSS.",
+      link: "https://imad273.github.io/Ditch-Portfolio/public/"
     }
   ]
 
@@ -34,20 +36,21 @@ const Projects = () => {
 
           <div className='my-12 grid grid-cols-3 gap-8'>
             {Projects.map(project => (
-              <div className='glass p-3'>
+              <div className='glass p-3 flex flex-col'>
                 <Image src={project.picture} alt={project.name} />
-                <div className='pt-3'>
-                  <h3 className='text-2xl text-slate-800 font-semibold'>{project.name}</h3>
-                  <p className='text-slate-800'>
-                    {project.description}
-                  </p>
+                <div className='flex flex-col gap-6 pt-3 h-full'>
+                  <div className='flex-1'>
+                    <h3 className='text-2xl text-slate-800 font-semibold'>{project.name}</h3>
+                    <p className='text-slate-800'>
+                      {project.description}
+                    </p>
+                  </div>
 
-                  <div className='flex justify-end pt-3 '>
-                    <div className='text-slate-700 hover:text-main'>
+                  <div className=''>
+                    <div className='text-slate-700 hover:text-main flex justify-end'>
                       <Link href={project.link} target='_blank' className='flex gap-2 items-center '>
                         <FaExternalLinkAlt /> View
                       </Link>
-
                     </div>
                   </div>
                 </div>
